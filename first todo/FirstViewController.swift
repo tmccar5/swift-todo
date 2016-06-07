f479a27741572602f9c13e5bfc7b223d78c72280
tmccar5 <tommccarthyguitar@gmail.com>
2016-06-07 12:22:05 -0500

    Initial Commit

---
+++ first todo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  first todo
+//
+//  Created by Tom McCarthy on 6/7/16.
+//  Copyright © 2016 Tom McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ first todoTests/first_todoTests.swift
@@ -0,0 +1,36 @@
+//
+//  first_todoTests.swift
+//  first todoTests
+//
+//  Created by Tom McCarthy on 6/7/16.
+//  Copyright © 2016 Tom McCarthy. All rights reserved.
+//
+
+import XCTest
+@testable import first_todo
+
+class first_todoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
