b3a7d657e3508292bc8da007c0dceffcf9f2238e
tmccar5 <tommccarthyguitar@gmail.com>
2016-06-07 22:07:53 -0500

    working form

--- first todo/FirstViewController.swift
+++ first todo/FirstViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.Delete
+        {
+            myItemList.removeAtIndex(indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myItemList.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "myCell")
+        myCell.textLabel?.text = myItemList[indexPath.row]
+        return myCell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        //print("viewDidAppear")
+        myTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print("viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
--- first todo/SecondViewController.swift
+++ first todo/SecondViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
+var myItemList = [String]()
+
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var myItemInput: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        if myItemInput.text?.characters.count > 0 {
+            myItemList.append(myItemInput.text!)
+            myItemInput.text = ""
+            self.view.endEditing(true)
+            print(myItemList)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
